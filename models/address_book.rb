ed569248bbaccd566db2beb388613cbed42ef3c1
Charles Peterson <nowledster@gmail.com>
2017-07-05 09:04:57 +0300

    Lines Reordered

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,22 +1,21 @@
 require_relative 'entry'
+
 class AddressBook
   attr_reader :entries
 
-    def initialize
+  def initialize
     @entries = []
-
   end
+
   def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
- end
+    # #9
+    index = 0
+    entries.each do |entry|
+      # #10
+      break if name < entry.name
+      index += 1
+    end
+    # #11
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
+end
