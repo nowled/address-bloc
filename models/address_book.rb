1c9bd3df92aa38f0b732be4e9351307b17a24c39
Charles Peterson <nowledster@gmail.com>
2017-07-15 14:42:15 +0300

    "Checkpoint 7  Complete"

--- models/address_book.rb
+++ models/address_book.rb
@@ -19,7 +19,7 @@ class AddressBook
   end
 
   def add_entry(name, phone_number, email)
-    # #9
+    # inserts items alphabetically
     index = 0
     entries.each do |entry|
       # #10
@@ -38,5 +38,30 @@ class AddressBook
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
+   end
+   # Search AddressBook for a specific entry by name
+   def binary_search(name)
+     lower = 0
+    upper = entries.length - 1
+
+    # #2
+    while lower <= upper
+      # #3
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+
+      # #4
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end  # #5
+    return nil
+   end
+
+   def iterative_search
    end
 end
